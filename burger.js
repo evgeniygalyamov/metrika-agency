81f266b7c5325467b211521bc416be9c355976f7
evgeniygalyamov <160492096+evgeniygalyamov@users.noreply.github.com>
2024-04-24 09:55:19 +0300

    Update burger.js

--- burger.js
+++ burger.js
@@ -2,14 +2,14 @@ const BURGER_OPENED_CLASSNAME_2 = 'burger_open';
 const BURGER_BTN_OPENED_CLASSNAME_2 = 'burger-btn_open';
 const BODY_FIXED_CLASSNAME_2 = 'body_fixed';
 
-const bodyNode = document.querySelector('body');
+const bodyNode2 = document.querySelector('body');
 const burgerNode = document.querySelector('.js-burger');
 const burgerBtnNode = document.querySelector('.js-burger-btn');
 const burgerContentNode = document.querySelector('.js-burger__content')
 
-burgerBtnNode2.addEventListener('click', toggleBurger);
+burgerBtnNode.addEventListener('click', toggleBurger);
 
-burgerNode2.addEventListener('click', (event) => {
+burgerNode.addEventListener('click', (event) => {
     const isClickOutsideContent = !event.composedPath().includes(burgerContentNode)
 
     if (isClickOutsideContent) {
@@ -22,4 +22,3 @@ function toggleBurger() {
     bodyNode.classList.toggle(BODY_FIXED_CLASSNAME_2);
     burgerBtnNode.classList.toggle(BURGER_BTN_OPENED_CLASSNAME_2);
 }
-
