78b746a37562dc5f467808c21055419c329d64b5
evgeniygalyamov <160492096+evgeniygalyamov@users.noreply.github.com>
2024-05-03 14:35:38 +0300

    Update burger.js

--- burger.js
+++ burger.js
@@ -5,7 +5,7 @@ const BODY_FIXED_CLASSNAME_2 = 'body_fixed';
 const bodyNode2 = document.querySelector('body');
 const burgerNode = document.querySelector('.js-burger');
 const burgerBtnNode = document.querySelector('.js-burger-btn');
-const burgerContentNode = document.querySelector('.js-burger__content')
+const burgerContentNode = document.querySelectorAll('.js-burger__content')
 
 burgerBtnNode.addEventListener('click', toggleBurger);
 
